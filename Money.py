8d33f868a6ecda0f35e7c740e4160084239ca3af
Ahmed Mohsen <xa.mohsen@gmail.com>
2018-09-01 12:01:14 +0200

    cleanup the repo

--- Money.py
+++ Money.py
@@ -1,6 +1,10 @@
+
 class Money:
     _amount = 0
 
     def __eq__(self, money):
-        print(type(self), "  ==> " , type(money))
+        print(type(self), "  ==> ", type(money))
         return self.amount == money.amount and type(self) == type(money)
+
+    def dollar(self, amount):
+        return Dollar(amount)
